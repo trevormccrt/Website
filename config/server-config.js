ae649a18562ffdc804e1766de3b3bb6f8af17da5
trevormccrt <trevor.mccourt@uwaterloo.ca>
2016-04-20 14:48:03 -0400

    not fixed

--- config/server-config.js
+++ config/server-config.js
@@ -2,7 +2,7 @@ var SERVER_SETTINGS={
 	serverPort:3000,
 	logRequests: true,
 	userCacheTTL: 1000*60*60*24*31, //in milliseconds (1 month)
-	emailSender: "postmaster@sandbox49104c24db824bf39593ac8da0ffd6a1.mailgun.org", //when a request is submitted in the contact form, this is the email that sends it
+	emailSender: "sandboxadb6400298ef46dab20b8fde539c47cf.mailgun.org", //when a request is submitted in the contact form, this is the email that sends it
 	emailSenderPassword: process.env.EMAIL_PASSWORD,
 	emailReceiver: "tj2mccou@uwaterloo.ca", //this is the email that the sender sends to in the contact form
 	google_analytics_id:"UA-68186801-1"
